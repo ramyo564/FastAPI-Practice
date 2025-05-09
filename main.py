77e1f219b03f09d6b05dcf3c888167e33d5d06b6
ramyo564 <yohan032yohan@gmail.com>
2025-05-09 22:55:52 +0900

    FastApi - path - ENUM 제어

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -8,11 +9,22 @@ def index():
     return "Hello World!"
 
 
+@app.get("/blog/all")
+def get_all_bolg():
+    return {"message": "All Blogs"}
+
+
 @app.get("/blog/{id}")
 def get_bolg(id: int):
     return {"message": f"Blog with id {id}"}
 
 
-@app.get("/blog/all")
-def get_all_bolg():
-    return {"message": "All Blogs"}
+class BlogType(str, Enum):
+    short = 'short'
+    story = "story"
+    howTo = "howTo"
+
+
+@app.get("/blog/type/{type}")
+def get_bolg_type(type: BlogType):
+    return {"message": f"Blog type {type.value}"}
