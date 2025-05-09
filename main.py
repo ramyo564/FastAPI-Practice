f4596412d9ead53d2682ce772c84ebbd526d3957
ramyo564 <yohan032yohan@gmail.com>
2025-05-09 22:45:10 +0900

    FastApi - path 순서
    순서대로 실행되기 때문에 해당 방법은 X

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "Hello World!"
+
+
+@app.get("/blog/{id}")
+def get_bolg(id: int):
+    return {"message": f"Blog with id {id}"}
+
+
+@app.get("/blog/all")
+def get_all_bolg():
+    return {"message": "All Blogs"}
