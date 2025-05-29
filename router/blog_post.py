47240bf823ede340d9208534c73014cf5e68925d
ramyo564 <yohan032yohan@gmail.com>
2025-05-29 21:00:02 +0900

    FastApi - Parameter metadata -> title , description , alias, deprecation

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Path, Body
 from pydantic import BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
@@ -15,3 +15,18 @@ class BlogModal(BaseModel):
 @router.post("/new/{id}")
 def create_blog(blog: BlogModal, id: int, version: int = 1):
     return {"id": id, "data": blog, "version": version}
+
+
+@router.post("/new/{id}/comment")
+def create_comment(
+    blog: BlogModal,
+    id: int,
+    comment_id: int = Query(
+        None,
+        title="Id of the comment",
+        description="Some description for comment_id",
+        deprecated=True
+        # alias="commentId",
+    ),
+):
+    return {"blog": blog, "id": id, "comment_id": comment_id}
