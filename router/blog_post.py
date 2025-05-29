d4f0ee5fcc7718b916f33c1682c25691151dfd66
ramyo564 <yohan032yohan@gmail.com>
2025-05-29 20:34:53 +0900

    FastApi - Request Body - pydantic
    Pydantic은 **타입 힌트(type hint)**를 기반으로 데이터를 검증하고 구조화해주는 라이브러리예요. BaseModel을 상속받아 클래스를 만들면, 딕셔너리 등 외부에서 들어온 데이터를 객체처럼 다루면서도, 그 값들이 올바른 형식인지 자동으로 확인할 수 있다.
    
    ✅ 언제, 어디서 쓸까?
    
        FastAPI에서 요청/응답 바디 정의할 때
    
        설정값(config) 파싱할 때
    
        외부 API로부터 받은 JSON을 객체로 변환할 때
    
        데이터를 구조화하고 타입 검증이 필요할 때

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,8 +1,17 @@
+from typing import Optional
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
 
+class BlogModal(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    pubished: Optional[bool]
+
+
 @router.post("/new")
-def create_blog():
-    pass
+def create_blog(blog: BlogModal):
+    return {"data": blog}
