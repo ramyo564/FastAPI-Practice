ef47ceaf8b59a94cbccb5364ce726a7aea0bf578
ramyo564 <yohan032yohan@gmail.com>
2025-06-01 00:23:40 +0900

    FastApi - Database - User / schemas / db / hash
    Import required libraries : sqlalchemy, passlib, bcrypt
    
    Create database definition and run it in main.py
    
    Create database models (tables)
    
    Create functionality to write to database
    
    Create schemas
    
     Data from user : UserBase
     Response to user : UserDisplay
    
    Create API operation

---
+++ router/user.py
@@ -0,0 +1,23 @@
+from typing import Optional, List, Dict
+from fastapi import APIRouter, Depends, Query, Path, Body
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from db import db_user
+from db.database import get_db
+from schemas import UserBase, UserDisplay
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+
+# Create User
+@router.post("/", response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
+
+
+# Read user
+
+# Update user
+
+# Delete user
