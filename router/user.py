a616a5c066deda15fe6a7710d3adfbc772db4cfb
ramyo564 <yohan032yohan@gmail.com>
2025-06-01 00:48:52 +0900

    FastApi - Update and Delete user

--- router/user.py
+++ router/user.py
@@ -29,5 +29,12 @@ def get_user(id: int, db: Session = Depends(get_db)):
 
 
 # Update user
+@router.post("/{id}/update")
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+    return db_user.update_user(db, id, request)
+
 
 # Delete user
+@router.get("/delete/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    return db_user.delete_user(db, id)
